c08758d84d52c0f30e6cc56a8a7f5ffc10bd5b7d
Lukas Hagman <lukas@geoguessr.com>
2022-12-05 21:59:05 +0100

    day 2 part 1

---
+++ day2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine(
+    String
+        .Join(" ", File.ReadAllLines("input.txt"))
+        .Split(" ")
+        .Select(x => x switch {
+            "A" or "X" => 0,
+            "B" or "Y" => 1,
+            "C" or "Z" => 2,
+            _ => throw new ArgumentOutOfRangeException()
+        })
+        .Chunk(2)
+        .Select(xs => ((3 + xs[1] - xs[0]) % 3) switch {
+            0 => 4 + xs[1],
+            1 => 7 + xs[1],
+            2 => 1 + xs[1],
+            _ => throw new ArgumentOutOfRangeException()
+        })
+        .Sum());
