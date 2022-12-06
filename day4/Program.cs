c869f5b0d0934a460709af525166179f80482ea5
Lukas Hagman <lukas@geoguessr.com>
2022-12-06 11:12:28 +0100

    day 4

---
+++ day4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(
+    File.ReadAllLines("input.txt")
+        .Select(line => line
+            .Split(",")
+            .Select(elf => elf
+                .Split("-")
+                .Select(int.Parse)))
+        // part 1:
+        // .Count(pair =>
+        //     pair.First().First() <= pair.Last().First() &&
+        //     pair.First().Last() >= pair.Last().Last() ||
+        //     pair.Last().First() <= pair.First().First() &&
+        //     pair.Last().Last() >= pair.First().Last()));
+        // part 2:
+        .Count(pair => !(
+            pair.First().Last()  < pair.Last().First() ^
+            pair.First().First() > pair.Last().Last() ||
+            pair.Last().Last()   < pair.First().First() ^
+            pair.Last().First()  > pair.First().Last())));
