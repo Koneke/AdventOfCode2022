066fb0a712058db932efe3c4bc7fbc16b4c8d650
Lukas Hagman <lukas@geoguessr.com>
2022-12-06 10:42:41 +0100

    part 2

--- day3/Program.cs
+++ day3/Program.cs
@@ -1,8 +1,14 @@
 ﻿Console.WriteLine(
     File.ReadAllLines("input.txt")
-        .SelectMany(r =>
-            r[..^(r.Length / 2)]
-                .Intersect(r[^(r.Length / 2)..])
-                .Distinct())
+        // part 1:
+        // .SelectMany(r =>
+        //     r[..^(r.Length / 2)]
+        //         .Intersect(r[^(r.Length / 2)..])
+        //         .Distinct())
+        // part 2:
+        .Chunk(3)
+        .SelectMany(chunk => chunk
+            .Aggregate<IEnumerable<char>>((p, n) => p.Intersect(n)))
+        // both:
         .Select(c => c - (c >= 97 ? 96 : 38))
         .Sum());
