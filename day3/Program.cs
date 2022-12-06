85d959f5a195ba27546abdae887254e0c31d2b3b
Lukas Hagman <lukas@geoguessr.com>
2022-12-06 10:35:55 +0100

    day 3 part 1

---
+++ day3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(
+    File.ReadAllLines("input.txt")
+        .SelectMany(r =>
+            r[..^(r.Length / 2)]
+                .Intersect(r[^(r.Length / 2)..])
+                .Distinct())
+        .Select(c => c - (c >= 97 ? 96 : 38))
+        .Sum());
