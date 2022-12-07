6d5de1caba1a1a0f2e11675b65d7b8fded7f2a9b
Lukas Hagman <lukas@geoguessr.com>
2022-12-07 15:08:55 +0100

    day 7

---
+++ day7/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine(
+    new[] {"input.txt"}
+        .Select(file =>
+            File.ReadAllLines(file)
+                .Aggregate((
+                        sizes: (IEnumerable<(string name, int size)>) new List<(string name, int size)> {("/", 0)},
+                        location: (IEnumerable<string>) new List<string> {"/"}),
+                    (current, next) =>
+                        next.StartsWith("$")
+                            ? next[2..] switch
+                            {
+                                "ls" => current,
+                                "cd /" => (current.sizes, location: new List<string> {"/"}),
+                                "cd .." => (current.sizes, location: current.location.SkipLast(1)),
+                                _ => (sizes: current.sizes.Append((
+                                        name: $"{string.Join("/", current.location)}/{next[5..]}",
+                                        size: 0)),
+                                    location: current.location.Append(next[5..]))
+                            }
+                            : next.Split(" ")[0].StartsWith("dir")
+                                ? current
+                                : (
+                                    sizes: current.sizes
+                                        .Select(dir => string.Join("/", current.location).StartsWith(dir.name)
+                                            ? (dir.name, size: dir.size + int.Parse(next.Split(' ')[0]))
+                                            : dir),
+                                    current.location)))
+        // part 1:
+        // .Select(result => result
+        //     .sizes
+        //     .Select(dir => dir.size)
+        //     .Where(size => size <= 100_000)
+        //     .Sum())
+        // .First()
+        // part 2:
+        .Select(result => result.sizes
+            .OrderBy(dir => dir.size)
+            .SkipWhile(dir => 70_000_000 - result.sizes.First(d => d.name == "/").size + dir.size < 30_000_000)
+            .First())
+        .First()
+        .size
+);
---
+++ day7/i-am-dumb-as-hell.cs
@@ -0,0 +1,38 @@
+﻿/*
+Console.WriteLine(
+    @"- / (dir)
+  - a (dir)
+    - e (dir)
+      - i (file, size=584)
+    - f (file, size=29116)
+    - g (file, size=2557)
+    - h.lst (file, size=62596)
+  - b.txt (file, size=14848514)
+  - c.dat (file, size=8504156)
+  - d (dir)
+    - j (file, size=4060174)
+    - d.log (file, size=8033020)
+    - d.ext (file, size=5626152)
+    - k (file, size=7214296)"
+        .Split("\n")
+        .Aggregate(
+            (sizes: (IEnumerable<(string name, int size)>) new List<(string, int)>(),
+                above: (IEnumerable<(string name, int depth)>) new List<(string, int)>()),
+            (state, next) => (
+                sizes: next.Contains("dir")
+                    ? state.sizes.Append((next.TrimStart().Split(" ")[1], 0))
+                    : state.sizes
+                        .Select(pair => state.above
+                            .TakeWhile(above => above.depth < next.Length - next.TrimStart().Length)
+                            .Any(parent => parent.name == pair.name)
+                            ? (pair.name, pair.size + int.Parse(next.Trim().Split('=')[1][..^1]))
+                            : pair),
+                above: next.Contains("dir")
+                    ? state.above
+                        .TakeWhile(above => above.depth < next.Length - next.TrimStart().Length)
+                        .Append((next.TrimStart().Split(" ")[1], next.Length - next.TrimStart().Length))
+                    : state.above))
+        .sizes
+        .Where(size => size.size <= 100_000)
+        .Sum(size => size.size));
+*/
